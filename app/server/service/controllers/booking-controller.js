e6c5bad6ea103818308dc690e014529e4fa182fc
Neha Girja <nehagirja09@gmail.com>
2025-07-21 12:55:38 +0530

    Add booking service API

---
+++ app/server/service/controllers/booking-controller.js
@@ -0,0 +1,123 @@
+import { request, response } from "express";
+import * as bookingService from "../services/booking-service.js";
+import * as UserService from "../services/user-service.js";
+import * as ServiceProviderService from "../services/service-provider-service.js";
+import * as ServiceTypeService from "../services/service-types.js";
+
+import {
+    setSuccess,
+    setError,
+    setBadRequest,
+    setUnauthorized,
+    setForbidden,
+    setInternalServerError,
+    setConflict,
+    setResourceCreated,
+    setNotFound
+} from "../utils/response-handlers.js";
+
+
+export const getAllBooking = async (request, response) => {
+    try {
+      const page = parseInt(request.query.page) || 1;
+      const count = parseInt(request.query.count) || 10;
+      const serviceProviderId = request.query.service_provider_id; // Get service_provider_id from query params
+      const user_id = request.query.user_id;
+      // Create a filter object
+      const filters = {};
+      if (serviceProviderId) {
+        filters.service_provider_id = serviceProviderId; // Add service_provider_id to filters
+      }
+      if(user_id){
+        filters.user_id = user_id;
+      }
+      
+
+      // const serviceRes = await bookingService.fetchAllBookings(page, count, filters);
+      // console.log("ServiceResponse : " + JSON.stringify(serviceRes));
+      // console.log("--------------------");
+
+      // const bookings = serviceRes.bookings;
+      // console.log("Bookings : " + JSON.stringify(bookings));
+      // console.log("--------------------");
+
+      const { bookings, metadata } = await bookingService.fetchAllBookings(page, count, filters);
+      // console.log("Bookings : " + JSON.stringify(bookings));
+      // console.log("Metadata : " + JSON.stringify(metadata));
+      
+      const totalRevenue = bookings.reduce((sum, booking) => sum + booking.total_amount, 0);
+      // console.log("Total Revenue : " + totalRevenue);
+      const completedBookings = metadata.completed;
+      const pendingAndConfirmedBooking = metadata.pendingAndConfirmed;
+
+      const totalBookings = await bookingService.getTotalBookingCount(filters);
+      const totalPages = Math.ceil(totalBookings / count);
+  
+      const responseData = {
+        metadata: {
+          total_items: totalBookings,
+          total_pages: totalPages,
+          total_revenue: totalRevenue,
+          current_page: page,
+          completedBookings: completedBookings,
+          pendingAndConfirmedBooking: pendingAndConfirmedBooking,
+        },
+        items: bookings,
+      };
+  
+      setSuccess(responseData, null, response);
+    } catch (error) {
+      setError(error, response);
+    }
+  };
+  
+export const initiateBooking = async(request,response) =>{
+    try {
+        const newBooking = { ...request.body };
+        const booking = await bookingService.save(newBooking);
+        setSuccess(booking, "Booking created Successfully", response);
+    } catch (error) {
+        setError(error, response);
+    }
+}
+
+export const cancelBooking = async(request,response) => {
+    try{
+        const { id } = request.params;
+        const updatedData = request.body; 
+        const booking = await bookingService.findByIdAndCancel(id,updatedData);
+        setSuccess(booking, "Booking cancelled Successfully", response);
+    }catch(error){
+        setError(error, response);
+    }
+}
+
+//getBooking
+
+export const getBookingById = async (request, response) => {
+    console.log("Get Booking by ID : ");
+    try {
+        const { id } = request.params;
+        const booking = await bookingService.findById(id);
+        
+        const UserData = await UserService.findUserById(booking.user_id);
+        console.log("UserData : " + JSON.stringify(UserData));
+
+        const bookingWithUserDetails = {
+          ...booking.toObject(), // Ensure toObject() is called if booking is a Mongoose document
+          UserData,
+      };
+
+        // const ServiceProviderData = await UserService.findUserById(booking.service_provider_id);
+        // console.log("ServiceProviderData : " + JSON.stringify(ServiceProviderData));
+
+        console.log("Booking : " + JSON.stringify(booking));
+        
+        setSuccess(bookingWithUserDetails,null, response);
+    }
+    catch (error) {
+        setError(error, response);
+    }
+};
+
+
