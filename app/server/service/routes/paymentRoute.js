c32eff09f01665b919a2ad26575de0bec3b0af8d
Neha Girja <nehagirja09@gmail.com>
2025-07-24 20:08:09 +0530

    Add gitignore for server

--- app/server/service/routes/paymentRoute.js
+++ app/server/service/routes/paymentRoute.js
@@ -3,7 +3,6 @@ import { createCheckoutSession } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
-// Route to create a Stripe Checkout session
 router.post("/create-checkout-session", createCheckoutSession);
 
 export default router;
