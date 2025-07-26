0f71a07c9841f238d4924a42c6ed3543ec2e8537
Neha Girja <nehagirja09@gmail.com>
2025-07-26 12:26:13 +0530

    Adding a custom hook for didMount

---
+++ app/client/src/utils/useDidMountEffect.ts
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react';
+
+const useDidMountEffect = (func: () => void, deps: React.DependencyList): void => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (didMount.current) {
+      func();
+    } else {
+      didMount.current = true;
+    }
+  }, deps);
+};
+
+export default useDidMountEffect;
